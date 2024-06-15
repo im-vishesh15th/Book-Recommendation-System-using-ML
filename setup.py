bc7843aeaf2dee309bb6a34230ab7a81cf1ca1d1
im-vishesh15th <145531271+im-vishesh15th@users.noreply.github.com>
2024-06-15 15:36:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-## edit below variables as per your requirements -
+# Variables
 REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
 AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = ['streamlit', 'numpy','pandas','scikit-learn','jupyter','matplotlib','seaborn']
-
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy', 'pandas', 'scikit-learn', 'jupyter', 'matplotlib', 'seaborn']
 
 setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for Movie Recommender System",
+    description="A small package for Books Recommender System",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email="entbappy73@gmail.com",
-    packages=[SRC_REPO],
+    packages=find_packages(where=SRC_REPO),
+    package_dir={"": SRC_REPO},
     license="MIT",
     python_requires=">=3.7",
     install_requires=LIST_OF_REQUIREMENTS
